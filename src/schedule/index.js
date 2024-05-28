bcc6f3a56e1050381a0132e212cfe3fc2f6600cd
Victor <victor3538@Gmail.com>
2024-05-28 15:50:57 +0900

    오전 10시 전송 코드 수정

--- src/schedule/index.js
+++ src/schedule/index.js
@@ -1,30 +1,35 @@
-// schedule/index.js
 const { getScheduleEmbed } = require('./scheduleEmbed.js');
 const { getData } = require('./scheduleFetcher.js');
 const { purgeChannel } = require('../utils.js');
 const path = require('path');
+const { DateTime } = require('luxon'); // luxon 모듈 추가
 const feel_logo = path.resolve(__dirname, '../../public/feel_logo.png');
 
 async function initializeScheduleManager(scheduleChannel) {
     await purgeChannel(scheduleChannel); // 동방시간표 채널 메시지 삭제
-    const data = await getData(); // 동방시간 데이터 가져오기
-    let scheduleMsg = await scheduleChannel.send({ embeds: [await getScheduleEmbed(data)], files: [feel_logo] }); // 채널에 메시지 전송
+    let scheduleMsg = await scheduleChannel.send({ embeds: [await getScheduleEmbed(await getData())], files: [feel_logo] }); // 채널에 메시지 전송
 
-    // 매일 아침 10시에 메시지 업데이트
-    const sendMessageAtTime = new Date();
-    sendMessageAtTime.setHours(10, 0, 0, 0); // 10시 0분 0초로 설정
-    const interval = sendMessageAtTime.getTime() - Date.now(); // 현재 시간과의 차이 계산
+    // 한국 시간대(KST) 기준으로 10시에 메시지 업데이트
+    const now = DateTime.now().setZone('Asia/Seoul'); // 현재 시간을 KST로 설정
+    const sendMessageAtTime = now.set({ hour: 10, minute: 0, second: 0, millisecond: 0 });
+
+    // 만약 현재 시간이 이미 10시를 지난 경우, 다음 날 10시로 설정
+    if (now > sendMessageAtTime) {
+        sendMessageAtTime = sendMessageAtTime.plus({ days: 1 });
+    }
+
+    const interval = sendMessageAtTime.diff(now).as('milliseconds'); // 현재 시간과의 차이 계산
 
     // interval 밀리초 후에 메시지 전송
     setTimeout(async () => {
         scheduleMsg.delete();
-        await scheduleChannel.send({ embeds: [await getScheduleEmbed()], files: [feel_logo] });
+        await scheduleChannel.send({ embeds: [await getScheduleEmbed(await getData())], files: [feel_logo] });
         console.log('새로운 날짜의 메시지를 전송했습니다.');
 
         // 24시간 후에 다시 실행
         setInterval(async () => {
             scheduleMsg.delete();
-            await scheduleChannel.send({ embeds: [await getScheduleEmbed()], files: [feel_logo] });
+            await scheduleChannel.send({ embeds: [await getScheduleEmbed(await getData())], files: [feel_logo] });
             console.log('새로운 날짜의 메시지를 전송했습니다.');
         }, 86400000); // 24시간 = 86400000밀리초
     }, interval);
