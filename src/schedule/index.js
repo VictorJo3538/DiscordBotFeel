2a07e99326be62d37773a366309eeb735f0d46a3
Victor <victor3538@Gmail.com>
2024-05-30 17:38:07 +0900

    날짜변경 변수 오류 수정

--- src/schedule/index.js
+++ src/schedule/index.js
@@ -2,7 +2,7 @@ const { getScheduleEmbed } = require('./scheduleEmbed.js');
 const { getData } = require('./scheduleFetcher.js');
 const { purgeChannel } = require('../utils.js');
 const path = require('path');
-const { DateTime } = require('luxon'); // luxon 모듈 추가
+const { DateTime } = require('luxon');
 const feel_logo = path.resolve(__dirname, '../../public/feel_logo.png');
 
 async function initializeScheduleManager(scheduleChannel) {
@@ -23,13 +23,13 @@ async function initializeScheduleManager(scheduleChannel) {
     // interval 밀리초 후에 메시지 전송
     setTimeout(async () => {
         scheduleMsg.delete();
-        await scheduleChannel.send({ embeds: [await getScheduleEmbed(await getData())], files: [feel_logo] });
+        scheduleMsg = await scheduleChannel.send({ embeds: [await getScheduleEmbed(await getData())], files: [feel_logo] });
         console.log('새로운 날짜의 메시지를 전송했습니다.');
 
         // 24시간 후에 다시 실행
         setInterval(async () => {
             scheduleMsg.delete();
-            await scheduleChannel.send({ embeds: [await getScheduleEmbed(await getData())], files: [feel_logo] });
+            scheduleMsg = await scheduleChannel.send({ embeds: [await getScheduleEmbed(await getData())], files: [feel_logo] });
             console.log('새로운 날짜의 메시지를 전송했습니다.');
         }, 86400000); // 24시간 = 86400000밀리초
     }, interval);
